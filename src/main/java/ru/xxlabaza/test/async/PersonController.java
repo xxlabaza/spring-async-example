b11907f15f55c98ae760bc32dc10eb7502100322
Artem Labazin <xxlabaza@gmail.com>
2016-01-21 01:30:50 +0300

    Initial commit

---
+++ src/main/java/ru/xxlabaza/test/async/PersonController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 xxlabaza.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.xxlabaza.test.async;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+/**
+ *
+ * @author Artem Labazin
+ * <p>
+ * @since Jan 16, 2016 | 6:43:37 PM
+ * <p>
+ * @version 1.0.0
+ */
+@RestController
+class PersonController {
+
+    private final static int BATCH_SIZE;
+
+    private final static int PARTITIONS;
+
+    static {
+        BATCH_SIZE = 5;
+        PARTITIONS = 5;
+    }
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping
+    @ResponseStatus(NO_CONTENT)
+    public void doAsync () {
+        List<Integer> ids = personService.findAllIdsInRange(0, BATCH_SIZE * PARTITIONS);
+        IntStream.range(0, PARTITIONS)
+                .forEach(index -> {
+                    int from = index * BATCH_SIZE;
+                    int to = Math.min(ids.size(), (index + 1) * BATCH_SIZE);
+                    personService.doAsync(ids.subList(from, to));
+                })
+                /*.map(personService::doAsync)*/;
+    }
+}
---
+++ src/main/java/ru/xxlabaza/test/async/PersonService.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 Pivotal Software, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.xxlabaza.test.async;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import static java.util.stream.Collectors.joining;
+
+/**
+ *
+ * @author Artem Labazin
+ * <p>
+ * @since Jan 20, 2016 | 11:09:38 PM
+ * <p>
+ * @version 1.0.0
+ */
+@Service
+class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Async("myThreadPoolTaskExecutor")
+    @SneakyThrows(InterruptedException.class)
+    public void doAsync (List<Integer> sequence) {
+        int delay = ThreadLocalRandom.current().nextInt(1, 6);
+        TimeUnit.SECONDS.sleep(delay);
+
+        if (sequence.contains(2)) {
+            throw new RuntimeException("Oh no! It contains 2!");
+        }
+
+        String ids = sequence.stream().map(Object::toString).collect(joining(", "));
+        String result = personRepository.findAll(sequence).stream().map(Person::toString).collect(joining(", "));
+
+        System.out.format("\n" +
+                "THREAD:      %s\n" +
+                "DELAYED:     %ds\n" +
+                "ID SEQUENCE: %s\n" +
+                "RESULT:      %s\n",
+                Thread.currentThread().getName(), delay, ids, result
+        );
+    }
+
+    public List<Integer> findAllIdsInRange (int page, int size) {
+        return personRepository.findAllIdsInRange(new PageRequest(page, size));
+    }
+}
